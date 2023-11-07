084b24cb04729fc8d2e7c5b5e9c39dab1b611add
Rahul Singh <rahul1905.sk@gmail.com>
2023-11-07 21:29:15 +0530

    backend2.0 added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { userRoutes } = require("./routes/User.routes");
 const { auth } = require("./middleware/auth.middleware");
 const { oemRoutes } = require("./routes/OEM.routes");
  const { dealerRoutes } = require("./routes/Dealer.routes");
+const { DealerModel } = require("./models/Dealer.model");
 require("dotenv").config();
 
 const app = express();
@@ -14,6 +15,19 @@ app.use(cors());
 app.use(express.json());
 app.use("/oem", oemRoutes);
 app.use("/users", userRoutes);
+
+
+app.get("/buyers", async (req, res) => {
+  try {
+    const data = await DealerModel.find();
+    console.log(data);
+    res.status(200).send({ msg: data });
+  } catch (error) {
+    res.status(400).send({ err: error.message });
+}
+});
+
+
 app.use(auth);
 
 app.use("/dealer", dealerRoutes)
